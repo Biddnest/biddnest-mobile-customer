7fe4bb2f29daaef33c59167b8d64f802046a7468
Akshita <akshitavadadoriya2505@gmail.com>
2021-02-26 15:59:03 +0530

    Setup onesignal

--- src/screens/auth/splash.js
+++ src/screens/auth/splash.js
@@ -1,9 +1,43 @@
 import React, {useEffect} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import OneSignal from 'react-native-onesignal';
 import {resetNavigator} from '../../constant/commonFun';
 import {Colors} from '../../constant/colors';
 
 const Splash = (props) => {
+  useEffect(() => {
+    OneSignal.setLogLevel(6, 0);
+
+    OneSignal.init('6b33862a-ec91-44bd-bf29-1c8ae35317d1', {
+      kOSSettingsKeyAutoPrompt: false,
+      kOSSettingsKeyInAppLaunchURL: false,
+      kOSSettingsKeyInFocusDisplayOption: 2,
+    });
+    OneSignal.inFocusDisplaying(2); // Controls what should happen if a notification is received while the app is open. 2 means that the notification will go directly to the device's notification center.
+
+    // The promptForPushNotifications function code will show the iOS push notification prompt. We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step below)
+    OneSignal.promptForPushNotificationsWithUserResponse(myiOSPromptCallback);
+
+    OneSignal.addEventListener('received', onReceived);
+    OneSignal.addEventListener('opened', onOpened);
+    OneSignal.addEventListener('ids', onIds);
+  }, []);
+  function myiOSPromptCallback(permission) {
+    // do something with permission value
+  }
+  function onReceived(notification) {
+    console.log('Notification received: ', notification);
+  }
+
+  function onOpened(openResult) {
+    console.log('Message: ', openResult.notification.payload.body);
+    console.log('Data: ', openResult.notification.payload.additionalData);
+    console.log('isActive: ', openResult.notification.isAppInFocus);
+    console.log('openResult: ', openResult);
+  }
+  function onIds(device) {
+    console.log('Device info: ', device);
+  }
   useEffect(() => {
     setTimeout(() => {
       resetNavigator(props, 'Login');
